4db10b9b8086ed10501d53f3b1f9a53f1cb63352
MostaphaEchelhi <mostapha.echelhi@medieinstitutet.se>
2023-03-06 19:13:16 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let usersList = document.getElementById("usersList");
 let newUser = document.getElementById("newUser");
+let newUserPassword = document.getElementById("newUserPassword")
 let saveUserBtn = document.getElementById("saveUserBtn");
+let loginUsername = document.getElementById("loginUsername");
+let loginPassword = document.getElementById("loginPassword")
+let loginUserBtn = document.getElementById("loginUserBtn");
+let UserGreeting = document.getElementById("UserGreeting");
 
 
 
@@ -10,6 +15,12 @@ fetch("http://localhost:3000/users")
     //console.log(data);
     printUsers(data);
 });    
+
+let loggedInUser = localStorage.getItem("username");
+
+if (loggedInUser) {
+    UserGreeting.innerText = "Godmorgon " + loggedInUser;
+}
 
 
 
@@ -27,7 +38,7 @@ function printUsers(users) {
 }
 
 saveUserBtn.addEventListener("click", () => {
-    let user = {name: newUser.value};
+    let user = {name: newUser.value, password: newUserPassword.value};
     console.log(user);
 
     fetch("http://localhost:3000/users", {
@@ -41,5 +52,37 @@ saveUserBtn.addEventListener("click", () => {
     .then(data => {
         printUsers(data);
     });
+    newUser.value = "";
+    newUserPassword.value = "";
 
 })
+
+loginUserBtn.addEventListener("click", () => {
+    
+    let loginUser = {
+        name: loginUsername.value,
+        password: loginPassword.value
+    } 
+    console.log(loginUser);
+
+    fetch("http://localhost:3000/users/login", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json", 
+    },
+    body: JSON.stringify(loginUser)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        if (data.name) {
+            UserGreeting.innerText = "Godmorgon " + data.name;
+            localStorage.setItem("username", data.name);
+        } else {
+            UserGreeting.innerText = "Fel lösenord eller användarnamn";
+        }
+    });
+    loginUsername.value = "";
+    loginPassword.value = "";
+
+});
